7f8674c33298712bc71ce8dca2c7effb34ca71e0
Jimmy <jimmy.brannon@barometerit.com>
2021-01-03 19:35:59 -0600

    span all floors and stop along the way, add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# [Elevator Saga](http://play.elevatorsaga.com/)
+
+Make sure to remove the `module.exports = ` code from the beginning before pasting code into the game,
+I added that so my IDE would stop yelling at me :)
+
+Top Score: **5**
+
--- index.js
+++ index.js
@@ -1,114 +1,86 @@
 module.exports = {
-  floorButtonsPressed: [],
+  floorButtonsPressedOnFloor: [],
   init(elevators, floors) {
-      const elevator = elevators[0]; // Let's use the first elevator
-      
-      this.addFloorListeners(floors, elevators);
-      this.addElevatorListeners(elevator);
+    const topFloor = floors[floors.length - 1];
+    //
+    elevators.forEach(elevator => {
+      //
+      this.addFloorListeners(floors);
+      this.addElevatorListeners(elevator, topFloor.level);
 
-      this.onFloorPress(elevator);
-      
+      // go to the very top right away - don't worry we'll make stops along the way!
+      elevator.goToFloor(topFloor.level);
+    });
   },
-  onFloorPress(elevator) {
-    console.log('============== ON FLOOR PRESS ================');
-    console.log(`CURRENT FLOOR: `, elevator.currentFloor());
-    console.log(`BUTTONS PRESSED ON FLOOR`, this.floorButtonsPressed);
-    //
-    const lowerQueuedFloors = this.getAllLowerQueuedFloors(elevator);               
-    const upperQueuedFloors = this.getAllUpperQueuedFloors(elevator); 
-    //
-    // If there are more stops above the current position, start moving up
-    if (upperQueuedFloors.length > lowerQueuedFloors.length) {
-      console.log(`MORE FLOORS ABOVE CURRENT FLOOR - MOVING UP TO FLOOR ${upperQueuedFloors[0]}`);
-      this.goToFloorIfValid(elevator, upperQueuedFloors[0], true);        
-    } else {
-      console.log(`MORE FLOORS BELOW CURRENT FLOOR - MOVING DOWN TO FLOOR ${lowerQueuedFloors[0]}`);
-      this.goToFloorIfValid(elevator, lowerQueuedFloors[0], true);          
-    }
-    console.log('-------------------------------------------');
-  },
-  addFloorListeners(floors, elevators) {
+  addFloorListeners(floors) {
     // add listeners to each floor
     floors.forEach(floor => {
       //
       floor.on("up_button_pressed", () => {
         console.log(`UP BUTTON PRESSED ON FLOOR ${floor.level}`);
-        this.floorButtonsPressed.push({ floorNum: floor.level, direction: 'up' });
-        this.onFloorPress(elevators[0]);
+        // Push floor into globql list that any elevator can stop if if it's going in the direction the user wants to go
+        this.floorButtonsPressedOnFloor.push({
+          floorNum: floor.level,
+          direction: 'up'
+        });
       });
       //
       floor.on("down_button_pressed", () => {
         console.log(`DOWN BUTTON PRESSED ON FLOOR ${floor.level}`);
-        this.floorButtonsPressed.push({ floorNum: floor.level, direction: 'down' });
-        this.onFloorPress(elevators[0]);
+        // Push floor into globql list that any elevator can stop if if it's going in the direction the user wants to go
+        this.floorButtonsPressedOnFloor.push({
+          floorNum: floor.level,
+          direction: 'down'
+        });
       });
       //
-  });
-  //
-  },
-  addElevatorListeners(elevator) {
+    });
     //
-    elevator.on('passing_floor', (floorNum, direction) => {
+  },
+  addElevatorListeners(elevator, topFloorLevel) {
+    //
+    elevator.on("passing_floor", (floorNum, direction) => {
       //
       console.log(`PASSING FLOOR ${floorNum} GOING ${direction}`);
       //
-      if (elevator.getPressedFloors().includes(floorNum)) {
+      const floorButtonWasPressedInElevator = elevator.getPressedFloors().includes(floorNum);
+      const floorButtonWasPressedOnFloorWithDirection = this.floorButtonsPressedOnFloor.filter(floorObj => floorObj.direction === direction).some(floorObj => floorObj.floorNum === floorNum);
+      //
+      // When coming up on a floor with we check a couple things...
+      // ...If someone on in the elevator wants to get off there, we stop
+      if (floorButtonWasPressedInElevator) {
         console.log(`PRESSED ELEVATOR FLOOR - STOPPING`);
         elevator.goToFloor(floorNum, true);
-      } else if (this.floorButtonsPressed.filter(floorObj => floorObj.direction === direction).some(floorObj => floorObj.floorNum === floorNum)) {
+
+        // ... If someone on the floor wants to go in the direction we're going, we stop 
+      } else if (floorButtonWasPressedOnFloorWithDirection) {
         console.log(`PRESSED FLOOR BUTTON - STOPPING`);
         elevator.goToFloor(floorNum, true);
-        this.floorButtonsPressed = this.floorButtonsPressed.filter(floorObj => floorObj.floorNum !== floorNum && floorObj.direction !== direction);
+        // We stopped here so remove floor from global list (until someone pressed a button again)
+        this.floorButtonsPressedOnFloor = this.floorButtonsPressedOnFloor.filter(
+          floorObj =>
+            floorObj.floorNum !== floorNum && floorObj.direction !== direction
+        );
       }
-
     });
     //
-    elevator.on("floor_button_pressed", (floorNum) => {
-      this.onFloorPress(elevator);
+    elevator.on("stopped_at_floor", floorNum => {
+      //
+      this.removeFloorFromFloorButtonPressedList(floorNum);
+      //
+      // constantly move from top floor to bottom floor - we stop at the appropiate floors on the way
+      if (floorNum === topFloorLevel) {
+        elevator.goToFloor(0);
+      } else if (floorNum === 0) {
+        elevator.goToFloor(topFloorLevel);
+      }
     });
-    //
-    elevator.on('stopped_at_floor', (floorNum) => {
-      this.removeFloorFromFloorButtonPressedList(floorNum);          
-    });
-  //
   },
   //
-  // Get all the queued floors below the current elevator position
-  // sort by closest to farthest from current position
-  getAllLowerQueuedFloors(elevator) {
-    const currentFloor = elevator.currentFloor();
-    const pressedFloors = elevator.getPressedFloors().filter(floor => floor < currentFloor);
-    const waitingFloors = this.floorButtonsPressed.map(floorObj => floorObj.floorNum);
-    return pressedFloors
-            .concat(waitingFloors)
-            .sort();
-  },  
-  //
-  // Get all the queued floors above the current elevator position
-  // sort by closest to farthest from current position
-  getAllUpperQueuedFloors(elevator) {
-    const currentFloor = elevator.currentFloor();
-    const pressedFloors = elevator.getPressedFloors().filter(floor => floor > currentFloor);
-    const waitingFloors = this.floorButtonsPressed.map(floorObj => floorObj.floorNum);
-    return pressedFloors
-            .concat(waitingFloors)
-            .sort()
-            .reverse();
-  },
-  //
-  goToFloorIfValid(elevator, floorNum, first = false) {
-    if (Number.isInteger(floorNum)) {
-      console.log('GOING TO FLOOR: ', floorNum);
-      elevator.goToFloor(floorNum, first);
-    }
-  },
-  //
-  // remove floors from list when we arrive there so we don't stop there again unneccarsarily 
+  // remove floors from list when we arrive there so we don't stop there again unneccarsarily
   removeFloorFromFloorButtonPressedList(floorNum) {
-    this.floorButtonsPressed = this.floorButtonsPressed.filter(floorObj => floorObj.floorNum !== floorNum);
+    this.floorButtonsPressedOnFloor = this.floorButtonsPressedOnFloor.filter(floorObj => floorObj.floorNum !== floorNum);
   },
-  //  
-  update(dt, elevators, floors) {
-      // We normally don't need to do anything here
-  }
+  //
+  update(dt, elevators, floors) {}
 }
